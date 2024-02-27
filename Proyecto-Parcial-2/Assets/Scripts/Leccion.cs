06b4fdf65b46fc1aa2809578da1d8dc79a4dd981
KarenVite <vitekaren89@gmail.com>
2024-02-26 19:31:53 -0600

    main - implement the Lesson Container Script

--- Proyecto-Parcial-2/Assets/Scripts/Leccion.cs
+++ Proyecto-Parcial-2/Assets/Scripts/Leccion.cs
@@ -16,5 +16,9 @@ public class Leccion
 
 public class Subject : ScriptableObject
 {
+    [Header("GameObject Configuration")]
+    public int Lesson = 0;
+
+    [Header("Lession Quest Configuration")]
     public List<Leccion> leccionList;
 }
---
+++ Proyecto-Parcial-2/Assets/Scripts/LessonContainer.cs
@@ -0,0 +1,68 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+
+public class LessonContainer : MonoBehaviour
+{
+    [Header("GameObject Configuration")]
+    public int Lection = 0;
+    public int CurrentLession = 0;
+    public int TotalLessions = 0;
+    public bool AreAllLessonsComplete = false;
+
+    [Header("UI Configuration")]
+    public TMP_Text StageTitle;
+    public TMP_Text LessonStage;
+
+    [Header("External GameObject Configuration")]
+    public GameObject lessonContainer;
+
+    [Header("Lesson Data")]
+    public ScriptableObject LessonData;
+
+    void Start()
+    {
+        if (lessonContainer != null)
+        {
+            OnUpdateUI();
+        }
+        else
+        {
+            Debug.LogWarning("GameObject Nulo, revisa las variables de tipo GameObject lessonContainer");
+
+        }
+    }
+
+    public void OnUpdateUI()
+    {
+        if (StageTitle != null || LessonStage != null)
+        {
+            StageTitle.text = "Leccion " + Lection;
+            LessonStage.text = "Leccion " + CurrentLession + " de " + TotalLessions;
+        }
+        else
+        {
+            Debug.LogWarning("GameObject Nulo, revisa las variables de tipo TMP_Text");
+        }
+    }
+
+    //Este metodo activa/desactiva la ventana de lessonContainer
+
+    public void EnableWindow()
+    {
+        OnUpdateUI();
+        if (lessonContainer.activeSelf)
+        {
+            //Desactiva el objeto si está activo
+            lessonContainer.SetActive(false);
+        }
+        else
+        {
+            //Activa el objeto si está desactivado
+            lessonContainer.SetActive(true);
+        }
+    }
+}
