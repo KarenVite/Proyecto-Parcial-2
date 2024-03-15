72cc34968853386374f3122d56ede5bbce2c20a9
KarenVite <vitekaren89@gmail.com>
2024-03-14 23:08:49 -0600

    Correccion 2

--- Proyecto-Parcial-2/Assets/Scripts/Subject.cs
+++ Proyecto-Parcial-2/Assets/Scripts/Subject.cs
@@ -2,15 +2,17 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-
+//Este Script es para configurar nuestro scriptableObject
 
 [CreateAssetMenu(fileName = "New Subject", menuName = "ScriptableObjects/New_Lesson", order = 1)]
 
 public class Subject : ScriptableObject
 {
     [Header("GameObject Configuration")]
+    //Definimos el número de nuestra Lección
     public int Lesson = 0;
 
     [Header("Lession Quest Configuration")]
+    //Para definir cuantas preguntas tendrá nuestra lección
     public List<Leccion> leccionList;
 }
